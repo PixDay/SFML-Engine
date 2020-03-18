e24a255ad4e1fc277cef072130313177ddb8700b
Adrien Colombier <adrien.colombier@epitech.eu>
2020-03-18 02:04:29 +0100

    [MODIFIED] - test show how to use a button to switch scene

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,15 +1,17 @@
 #include "SFML-Engine.h"
 #include <iostream>
 
+SceneManager sceneManager;
+
 void test(void)
 {
-    std::cout << "Hello function handler" << std::endl;
+    sceneManager.setCurrentScene("game");
+    sceneManager.setEngineCursor();
 }
 
 int main(void)
 {
-    SceneManager sceneManager;
-
+    sceneManager.addScene("game");
     sceneManager.setEngineCursor();
     sceneManager.addObject(new ActionButton(test, 100.0f, 100.0f));
     sceneManager.update();
