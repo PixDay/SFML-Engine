ee85adb208fbf68151225fdb28fad72c00125caf
Adrien Colombier <adrien.colombier@epitech.eu>
2020-04-05 00:51:34 +0200

    [MODIFIED] - installer version beta ready

--- script/parameter.sh
+++ script/parameter.sh
@@ -2,7 +2,40 @@
 
 function main()
 {
-    echo "SFE command is not working currently"
+    if [[ $1 = "update" ]]; then
+        update
+    elif [[ $1 = "--help" ]]; then
+        help
+    elif [[ $1 = "create" ]]; then
+        create_project $2
+    fi
 }
 
-main
+function update()
+{
+    cd /bin/SFML-Engine
+    sudo git pull
+    echo "Engine updated"
+    cp script/parameter.sh /bin/SFE
+    sudo chmod 777 /bin/SFE
+    echo "SFE updated"
+    cd
+}
+
+function help()
+{
+    echo "SFE command :"
+    echo "    --help : show you the different available commands"
+    echo "    update : updating the SFML-Engine and the SFE commands (can ask for password)"
+    echo "    create [name] : create a directory with the given name that will contain the default file project for the SFML-Engine"
+}
+
+function create_project()
+{
+    mkdir $1
+    cd $1
+    cp -r /bin/SFML-Engine/* .
+    echo "SFML-Engine project created !"
+}
+
+main $1 $2
